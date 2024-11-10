744d9697fd161f88e8cebb0fc33bce8475de4b49
Reynaldo Gil <gilcu2@gmail.com>
2024-11-10 15:21:44 +0100

    coverage

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.15.0")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.8")
---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,27 @@
+import org.scalatest.GivenWhenThen
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers.*
+
+class MainSpec extends AnyFunSpec with GivenWhenThen {
+
+  it("compute the minimal path from text lines") {
+    Given("graph")
+    val lines =
+      """
+        |7
+        |6 3
+        |3 8 5
+        |11 2 10 9
+        |""".stripMargin.split("\n").filter(_.nonEmpty).iterator
+
+    And("expected result")
+    val expected = "7 + 6 + 3 + 2 = 18"
+
+    When("compute")
+    val r = computeMinimalPath(lines)
+
+    Then("it is the expected")
+    r mustBe expected
+  }
+
+}
